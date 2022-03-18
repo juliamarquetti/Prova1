3d043790e758bf2cd9263f3d6b552416d9e8f341
julia <julia@LAPTOP-6P43C68K>
2022-03-18 20:16:45 -0300

    Prova 1 Questão 1

---
+++ src/br/ulbra/classes/Scanner.java
@@ -0,0 +1,12 @@
+package br.ulbra.classes;
+
+import java.io.InputStream;
+
+public class Scanner 
+{
+
+    Scanner(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
